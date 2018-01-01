6c088d7473f14d00cb8cb02f327c7fc74369477d
Brian Brunner <brian.brunner@gmail.com>
2017-12-31 19:09:40 -0700

    TMP SAVE

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var path = require('path'),
     url = require('url'),
+    redis = require('redis'),
     WebSocket = require('ws');
 
 var bot_name = "";
@@ -8,8 +9,24 @@ var platform = module.exports = function(options) {
 
   var options = options || {};
   var client_url = options.client_url || "/socket/client.js";
+  var pubsub_prefix = options.pubsub_prefix || "__yowl_ws_pubsub__"
+
+  if (options.redis) {
+    var redisSubClient = redis.createClient(options.redis);
+    redisSubClient.on('message', function(channel, message) {
+      var sessionId = channel.spli(":")[1];
+      var data = JSON.parse(message);
+      var ws = sockets[sessionId];
+      if (ws) {
+        send_response(ws, data);
+      }
+    });
+    var redisPubClient = redisSubClient.duplicate();
+  }
 
   var initFn = function(bot) {
+
+    pubsub_prefix += bot.display_name;
 
     bot.app.get(client_url, function(req, res) {
       res.sendFile(path.resolve(__dirname, 'client.js'));
@@ -40,6 +57,8 @@ var platform = module.exports = function(options) {
         }
       }
 
+      redisSubClient.subscribe(pubsub_prefix+":"+sessionId);
+
       ws.on('message', function(jsonBlob) {
         var parsed = JSON.parse(jsonBlob);
         bot(platform, { sessionId: sessionId, __ws: ws }, { type: 'message', message: parsed.message }, function(err, context, event, cb) {
@@ -54,6 +73,7 @@ var platform = module.exports = function(options) {
 
       ws.on('close', function() {
         delete sockets[sessionId];
+        redisSubClient.unsubscribe(pubsub_prefix+":"+sessionId);
       });
 
     });
@@ -69,16 +89,22 @@ var platform = module.exports = function(options) {
 // Define our platform characteristics
 
 platform.id = "ws"
-platform.capabilities = ["say", "actions"]
+platform.capabilities = ["say", "actions", "typing"]
 platform.send = function(context, event, response, cb) {
   ws = context.__ws;
   if (!ws) {
     ws = sockets[context.sessionId];
     if (!ws) {
-      if (!context.session.__pending) {
-        context.session.__pending = [];
+      var jsonRes = JSON.stringify(response);
+      redisPubClient.publish(pubsub_prefix+":"+context.sessionId, jsonRes, function(err, count) {
+        if (err || count == 0) {
+          if (!context.session.__pending) {
+            context.session.__pending = [];
+          }
+          context.session.__pending.push(response);
+        }
+        cb(err, context, event, response);
       }
-      context.session.__pending.push(response);
     }
   }
 
@@ -90,9 +116,9 @@ platform.send = function(context, event, response, cb) {
     responses.forEach(function(res) {
       send_response(ws, res);
     });
+    cb(null, context, event, response);
   }
 
-  cb(null, context, event, response);
 }
 
 function send_response(ws, response) {
